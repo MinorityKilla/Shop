06c1dc3072d2e95d2adc18bb0d325ee49def1850
MinorityKilla <pavelzel1007@gmail.com>
2026-01-21 00:16:29 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 public class Main {
+    // Single Responsibility Principle: класс отвечает только за запуск приложения
     public static void main(String[] args) {
         new Shop().run();
     }
