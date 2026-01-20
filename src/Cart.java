4152b7cb443391eb02625687994f5d94120b98cb
MinorityKilla <pavelzel1007@gmail.com>
2026-01-21 00:26:50 +0300

    Update Cart.java

--- src/Cart.java
+++ src/Cart.java
@@ -2,6 +2,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Cart {
+
+    // SRP: класс отвечает только за корзину
     private final List<Product> products = new ArrayList<>();
 
     public List<Product> getAll() {
