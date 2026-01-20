24ebaa2f4fe23c5990e4958c8ccc0de29ef4a929
MinorityKilla <pavelzel1007@gmail.com>
2026-01-21 00:25:13 +0300

    Update Catalog.java

--- src/Catalog.java
+++ src/Catalog.java
@@ -1,6 +1,8 @@
 import java.util.List;
 
 public class Catalog {
+
+     // Single Responsibility Principle: класс отвечает только за хранение и вывод товаров
     private final List<Product> products = List.of(
             new Product("Телефон", 500),
             new Product("Ноутбук", 1200),
@@ -8,10 +10,17 @@ public class Catalog {
     );
 
     public void show() {
+
+         // Don't Repeat Yourself: единый способ вывода товаров
         products.forEach(System.out::println);
     }
 
     public void showFiltered(ProductFilter filter) {
+
+        // OCP:
+        // каталог не изменяется при добавлении новых фильтров
+        // LSP:
+        // любая реализация ProductFilter может быть подставлена
         products.stream().filter(filter::check).forEach(System.out::println);
     }
 }
